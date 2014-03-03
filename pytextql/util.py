83bbc5fdc4efaf4c176052d5b90a3171e3a2d3e3
Tyler Kennedy <tk@tkte.ch>
2014-03-03 18:35:56 -0500

    Very first iteration of pytextql. Functional, but not fancy.

---
+++ pytextql/util.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import csv
+import itertools
+
+
+def grouper(iterable, n):
+    """
+    Slice up `iterable` into iterables of `n` items.
+
+    :param iterable: Iterable to splice.
+    :param n: Number of items per slice.
+    :returns: iterable of iterables
+    """
+    it = iter(iterable)
+    while True:
+        chunk = itertools.islice(it, n)
+        try:
+            first = next(chunk)
+        except StopIteration:
+            return
+
+        yield itertools.chain([first], chunk)
+
+
+class UnicodeCSVReader(object):
+    """
+    An extremely minimal wrapper around csv.reader to assist in
+    reading Unicode data.
+    """
+    def __init__(self, *args, **kwargs):
+        self.encoding = kwargs.pop('encoding', 'utf8')
+        self.pad_to = kwargs.pop('pad_to', 0)
+        self.pad_with = kwargs.pop('pad_with', '')
+        self.reader = csv.reader(*args, **kwargs)
+
+    def next(self):
+        row = self.reader.next()
+        padding = [self.pad_with] * (self.pad_to - len(row))
+        return [unicode(c, self.encoding) for c in row] + padding
+
+    def __iter__(self):
+        return self
+
+    @property
+    def dialect(self):
+        return self.reader.dialect
+
+    @property
+    def line_num(self):
+        return self.reader.line_num
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+import os.path
+from setuptools import setup, find_packages
+
+
+def get_version():
+    """
+    Loads the current module version from version.py and returns
+    it.
+
+    :returns: module version identifier.
+    :rtype: str
+    """
+    local_results = {}
+    version_file_path = os.path.join('pytextql', 'version.py')
+
+    # This is compatible with py3k which removed execfile.
+    with open(version_file_path, 'rb') as fin:
+        # Compiling instead of passing the text straight to exec
+        # associates any errors with the correct file name.
+        code = compile(fin.read(), version_file_path, 'exec')
+        exec(code, {}, local_results)
+
+    return local_results['__version__']
+
+
+if __name__ == '__main__':
+    setup(
+        name='pytextql',
+        version=get_version(),
+        long_description=__doc__,
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=[
+            'docopt'
+        ],
+        scripts=[
+            'pytextql/pytextql'
+        ]
+    )
